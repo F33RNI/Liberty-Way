917251f49ce3c434e9ed636e66be56210beb2e7d
Frey Hertz <39090123+XxOinvizioNxX@users.noreply.github.com>
2021-02-10 10:54:42 -0500

    Add files via upload

---
+++ START_LINUX.sh
@@ -0,0 +1,7 @@
+set echo off
+echo Java home: $JAVA_HOME
+java -version ""
+echo ""
+java -Djava.library.path=. -jar "./Liberty-Way.jar" -c ""
+read -s -n 1 -p "Press any key to continue . . ."
+echo ""
