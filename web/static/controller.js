4cfe8a5582bacc6a31b9f897ab131a7d34561903
Frey Hertz <xxoinvizionxx@gmail.com>
2021-05-13 12:39:59 -0500

    Add files via upload

--- web/static/controller.js
+++ web/static/controller.js
@@ -1,3 +1,26 @@
+/*
+ * Copyright (C) 2021 Frey Hertz (Pavel Neshumov), Liberty-Way Landing System Project
+ *
+ * This software is part of Autonomous Multirotor Landing System (AMLS) Project
+ *
+ * Licensed under the GNU Affero General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/agpl-3.0.en.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 const bingAPIKey = "AnBO2H6OMskUam6VW9qK2-kD6dfzYFTH1hIXDsjRbsFfUbKX4SubZV8WnME0sntE";
 let mapEnabled = false;
 
@@ -226,7 +249,7 @@ function abort() {
 		if (this.readyState === 4 && this.status === 200 && JSON.parse(this.responseText).status.toLowerCase() === "ok")
 			window.location.href = "/";
 	};
-	
+
 	xmlHTTP.open("POST", "/api", true);
 	xmlHTTP.setRequestHeader("Content-Type", "application/json");
 	xmlHTTP.send(JSON.stringify({ "action": "abort" }));
