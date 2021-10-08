bed5c70adac5f7f4fabc2cc26aaeefa8a5afbb19
XxOinvizioNxX <xxoinvizionxx@gmail.com>
2021-10-08 20:03:30 +0300

    Fix copyright info

--- GPS-mixer/datatypes.h
+++ GPS-mixer/datatypes.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Fern H. (Pavel Neshumov), Liberty Drones GPS mixer
+ * Copyright (C) 2021 Fern H. (aka Pavel Neshumov), Liberty Drones GPS mixer
  *
  * Licensed under the GNU Affero General Public License, Version 3.0 (the "License");
  * you may not use this file except in compliance with the License.
