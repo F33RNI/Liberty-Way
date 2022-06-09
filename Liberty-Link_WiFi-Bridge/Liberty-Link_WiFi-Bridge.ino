0f361e03c37f61dac513377ae7cc20d77ffc11a6
Yas-lab <79026153+Yas-lab@users.noreply.github.com>
2022-06-09 22:37:22 +0300

    Update copyright info

--- Liberty-Link_WiFi-Bridge/Liberty-Link_WiFi-Bridge.ino
+++ Liberty-Link_WiFi-Bridge/Liberty-Link_WiFi-Bridge.ino
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2021 Fern H. (aka Pavel Neshumov), Liberty-Link Wifi bridge
+* Copyright (C) 2021 Vladislav Yasnetsky, Liberty-Link Wifi bridge
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
